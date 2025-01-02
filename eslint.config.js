567b16ae2d7b1e1826b3293adf36cc4eab5b4a5f
tranvudpqn123 <tranvudpqn123@gmail.com>
2025-01-02 12:02:36 +0700

    configuration: set eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -4,40 +4,63 @@ const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
 
 module.exports = tseslint.config(
-  {
-    files: ["**/*.ts"],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.stylistic,
-      ...angular.configs.tsRecommended,
-    ],
-    processor: angular.processInlineTemplates,
-    rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
-        {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+    {
+        files: ["**/*.ts"],
+        extends: [
+            eslint.configs.recommended,
+            ...tseslint.configs.recommended,
+            ...tseslint.configs.stylistic,
+            ...angular.configs.tsRecommended,
+        ],
+        processor: angular.processInlineTemplates,
+        rules: {
+            "no-duplicate-imports": "error",
+            "no-console": "warn",               // Warn on console.log usage
+            "no-debugger": "error",             // Disallow the use of debugger
+            "eqeqeq": ["error", "always"],      // Enforce strict equality (=== and !==)
+            "curly": ["error", "all"],          // Require braces around blocks
+            "no-unused-vars": ["warn", {
+                "args": "none",
+                "ignoreRestSiblings": true
+            }],                                // Warn on unused variables
+            "no-undef": "error",                // Disallow the use of undeclared variables
+            "prefer-const": "warn",             // Suggest using const for variables that are not reassigned
+            "no-var": "error",                  // Disallow var, prefer let/const
+            "semi": ["error", "always"],        // Require semicolons
+            "quotes": ["error", "single"],      // Enforce single quotes
+            "indent": ["error", 4],             // Enforce 2 spaces for indentation
+            "comma-dangle": ["error", "never"],  // Disallow trailing commas,
+            "@typescript-eslint/explicit-function-return-type": "warn", // Require explicit return types
+            "@typescript-eslint/no-explicit-any": "warn",               // Warn on usage of `any`
+            "@typescript-eslint/no-unused-vars": ["warn"],              // Warn on unused variables
+            "@typescript-eslint/no-non-null-assertion": "error",        // Disallow non-null assertions
+            "@typescript-eslint/no-inferrable-types": "warn",           // Warn when type can be inferred
+            "@typescript-eslint/consistent-type-definitions": ["error", "interface"], // Prefer interface over type
+            "@typescript-eslint/no-empty-function": "warn",
+            "@angular-eslint/directive-selector": [
+                "error",
+                {
+                    type: "attribute",
+                    prefix: "app",
+                    style: "camelCase",
+                },
+            ],
+            "@angular-eslint/component-selector": [
+                "error",
+                {
+                    type: "element",
+                    prefix: "app",
+                    style: "kebab-case",
+                },
+            ],
         },
-      ],
-      "@angular-eslint/component-selector": [
-        "error",
-        {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
-        },
-      ],
     },
-  },
-  {
-    files: ["**/*.html"],
-    extends: [
-      ...angular.configs.templateRecommended,
-      ...angular.configs.templateAccessibility,
-    ],
-    rules: {},
-  }
+    {
+        files: ["**/*.html"],
+        extends: [
+            ...angular.configs.templateRecommended,
+            ...angular.configs.templateAccessibility,
+        ],
+        rules: {},
+    }
 );
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'cpl-crm';
+export class AppComponent implements OnInit{
+    title = 'cpl-crm';
+
+    ngOnInit() {
+        console.log('App component initialized', this.title);
+    }
 }
